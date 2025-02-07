2cf0b5da4aec684925f26d65db5da1bb01357289
realJoey <gouldj@vt.edu>
2025-02-07 13:18:35 -0500

    Update component.py

--- flight_simulator/core/vehicle/component.py
+++ flight_simulator/core/vehicle/component.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from flight_simulator.core.loads.mass_properties import MassProperties
-
+import numpy as np
 from lsdo_geo import Geometry
 from lsdo_function_spaces import FunctionSet
 from typing import Union
@@ -243,3 +243,91 @@ class Component:
         surface_area = surface_area + wireframe_area
 
         return surface_area
+    
+    
+    def connect_component_geometries(
+            self, comp_1: "Component", comp_2: "Component",
+            connection_point: Union[csdl.Variable, np.ndarray, None] = None):
+        csdl.check_parameter(comp_1, "comp_1", types=Component)
+        csdl.check_parameter(comp_2, "comp_2", types=Component)
+        csdl.check_parameter(connection_point, "connection_point", types=(csdl.Variable, np.ndarray), allow_none=True)
+
+        if comp_1.geometry is None:
+            raise Exception(f"Comp {comp_1._name} does not have a geometry.")
+        if comp_2.geometry is None:
+            raise Exception(f"Comp {comp_2._name} does not have a geometry.")
+
+        if connection_point is not None:
+            try:
+                connection_point.reshape((3,))
+            except:
+                raise Exception(f"'connection_point' must be of shape (3, ) or reshapable to (3, ). Received shape {connection_point.shape}")
+
+            projection_1 = comp_1.geometry.project(connection_point)
+            projection_2 = comp_2.geometry.project(connection_point)
+
+            self._geometric_connections.append((projection_1, projection_2, comp_1, comp_2))
+
+        else:
+            point_1 = comp_1.ffd_block.evaluate(parametric_coordinates=np.array([0.5, 0.5, 0.5]))
+            point_2 = comp_2.ffd_block.evaluate(parametric_coordinates=np.array([0.5, 0.5, 0.5]))
+
+            projection_1 = comp_1.geometry.project(point_1)
+            projection_2 = comp_2.geometry.project(point_2)
+
+            self._geometric_connections.append((projection_1, projection_2, comp_1, comp_2))
+
+    def setup_geometry(self, run_ffd: bool = True, plot: bool = False):
+        self._geometry_setup_has_been_called = True
+
+        if self._is_copy and run_ffd:
+            raise Exception("Cannot call setup_geometry with run_ffd=True on a copy of a configuration.")
+
+        from lsdo_geo.core.parameterization.parameterization_solver import ParameterizationSolver, GeometricVariables
+
+        parameterization_solver = ParameterizationSolver()
+        ffd_geometric_variables = GeometricVariables()
+        system_geometry = self.system.geometry
+
+        if system_geometry is None:
+            raise TypeError("'setup_geometry' cannot be called because the geometry associated with the system component is None")
+
+        if not isinstance(system_geometry, FunctionSet):
+            raise TypeError(f"The geometry of the system must be of type {FunctionSet}. Received {type(system_geometry)}")
+
+        def setup_geometries(component: "Component"):
+            component._skip_ffd = False
+            component._skip_ffd = False
+            if component.geometry is not None:
+                if component._skip_ffd is True:
+                    pass
+                else:
+                    try:
+                        component._setup_geometry(parameterization_solver, ffd_geometric_variables, plot=plot)
+                    except NotImplementedError:
+                        warnings.warn(f"'_setup_geometry' has not been implemented for component {component._name} of {type(component)}")
+
+            if component.comps:
+                for comp in component.comps.values():
+                    setup_geometries(comp)
+
+        setup_geometries(self.system)
+
+        for projection_1, projection_2, comp_1, comp_2 in self._geometric_connections:
+            if isinstance(projection_1, list):
+                connection = comp_1.geometry.evaluate(parametric_coordinates=projection_1) - comp_2.geometry.evaluate(parametric_coordinates=projection_2)
+            elif isinstance(projection_1, np.ndarray):
+                connection = comp_1.ffd_block.evaluate(parametric_coordinates=projection_1) - comp_2.ffd_block.evaluate(parametric_coordinates=projection_2)
+            else:
+                raise NotImplementedError(f"Unsupported projection type {type(projection_1)}")
+
+            ffd_geometric_variables.add_variable(connection, connection.value)
+
+        t1 = time.time()
+        parameterization_solver.evaluate(ffd_geometric_variables)
+        t2 = time.time()
+        print(f"Time for inner optimization: {t2 - t1}")
+
+        if plot:
+            system_geometry.plot(show=True)
+
