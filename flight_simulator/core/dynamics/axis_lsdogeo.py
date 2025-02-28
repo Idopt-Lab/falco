e7172421c338b9597e4531bd8be11aeb69861ea3
realJoey <gouldj@vt.edu>
2025-02-28 12:24:57 -0500

    Update axis_lsdogeo.py

--- flight_simulator/core/dynamics/axis_lsdogeo.py
+++ flight_simulator/core/dynamics/axis_lsdogeo.py
@@ -23,3 +23,12 @@ class AxisLsdoGeo(Axis):
                          phi=phi, theta=theta, psi=psi,
                          origin=origin, sequence=sequence, reference=reference)
         
+
+    @property
+    def translation(self):
+        return self.geometry.evaluate(self.parametric_coords)
+
+
+    @translation.setter
+    def translation(self, translation_vector):
+        pass
