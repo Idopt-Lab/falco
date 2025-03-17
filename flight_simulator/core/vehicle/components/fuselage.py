052f6ff0818d508838ae6b8fb5cb9949da1b26fa
Nate Shune <106626400+nshune@users.noreply.github.com>
2025-03-17 14:52:32 -0400

    Update fuselage.py

--- flight_simulator/core/vehicle/components/fuselage.py
+++ flight_simulator/core/vehicle/components/fuselage.py
@@ -241,7 +241,3 @@ class Fuselage(Component):
         else:
             ffd_geometric_variables.add_variable(fuselage_geometric_qts.width, width_outer_dv)
 
-
-        print('Computed Fuselage Length',fuselage_geometric_qts.length.value)
-        print('Computed Fuselage Width',fuselage_geometric_qts.width.value)
-        print('Computed Fuselage Height',fuselage_geometric_qts.height.value)
