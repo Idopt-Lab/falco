60400d87e1cf875511ee2dc17dc91eb798dbb658
realJoey <gouldj@vt.edu>
2025-04-06 20:01:47 -0400

    Update test_component.py

--- flight_simulator/core/vehicle/tests/test_component.py
+++ flight_simulator/core/vehicle/tests/test_component.py
@@ -6,10 +6,14 @@ import csdl_alpha as csdl
 import numpy as np
 
 from flight_simulator.core.dynamics.axis import Axis, ValidOrigins
+from flight_simulator.core.dynamics.axis_lsdogeo import AxisLsdoGeo
 from flight_simulator.core.loads.forces_moments import Vector
 from flight_simulator.core.loads.mass_properties import MassProperties, MassMI
-from flight_simulator.core.component import Component, ComponentQuantities, ComponentParameters
+from flight_simulator.core.vehicle.component import Component, ComponentQuantities, ComponentParameters
 from flight_simulator.utils.import_geometry import import_geometry
+from flight_simulator.core.vehicle.models.aerodynamics.aerodynamic_model import LiftModel
+from flight_simulator.core.dynamics.aircraft_states import AircraftStates
+from flight_simulator.core.vehicle.aircraft_control_system import AircraftControlSystem
 
 
 class TestComponentInitialization(TestCase):
@@ -45,32 +49,7 @@ class TestComponentInitialization(TestCase):
         component = Component(name="custom_component")
         self.assertEqual(component.comps, {})
 
-    # def test_providing_mp(self):
-    #     component = Component(name="custom_component")
-
-        axis = Axis(
-            name='Inertial Axis',
-            x=np.array([0]) * ureg.meter,
-            y=np.array([0]) * ureg.meter,
-            z=np.array([0]) * ureg.meter,
-            phi=np.array([0, ]) * ureg.degree,
-            theta=np.array([0, ]) * ureg.degree,
-            psi=np.array([0, ]) * ureg.degree,
-            origin=ValidOrigins.Inertial.value
-        )
-
-        # Create a Quantity vector with units
-        cg = Q_([0, 0, 0], 'newton')
-        cg = Vector(cg, axis)
-        # Create a mass moment of inertia object
-        mi = MassMI(axis=axis)
-        # Create a mass properties object
-        mp = MassProperties(cg=cg, inertia=mi, mass=Q_(10, 'lb'))
-
-    #     component.quantities.mass_properties = mp
-    #     np.testing.assert_equal(component.quantities.mass_properties.inertia_tensor.inertia_tensor.value,
-    #                             np.zeros((3, 3)))
-    #     np.testing.assert_almost_equal(component.quantities.mass_properties.mass.value, desired=4.53592, decimal=5)
+ 
 
 
 class TestComponentGeometry(TestCase):
@@ -143,3 +122,78 @@ class TestComponentHierarchy(TestCase):
         (Path.cwd()/"python_test_outputs/component_hierarchy.png").unlink()
         (Path.cwd()/"python_test_outputs/component_hierarchy").unlink()
         (Path.cwd() / "python_test_outputs/").rmdir()
+
+
+
+
+class TestComponentMassProperties(TestCase):
+    def setUp(self):
+        recorder = csdl.Recorder(inline=True)
+        recorder.start()
+
+        self.axis = Axis(
+            name='Inertial Axis',
+            x=np.array([0]) * ureg.meter,
+            y=np.array([0]) * ureg.meter,
+            z=np.array([0]) * ureg.meter,
+            phi=np.array([0, ]) * ureg.degree,
+            theta=np.array([0, ]) * ureg.degree,
+            psi=np.array([0, ]) * ureg.degree,
+            origin=ValidOrigins.Inertial.value
+        )
+    
+
+        self.parent_component = Component(name="Aircraft")
+        self.sub_component1 = Component(name="Wing")
+        self.sub_component2 = Component(name="Motor")
+        self.sub_component3 = Component(name="Fuselage")
+
+
+        # Add subcomponents to the parent component
+        self.parent_component.add_subcomponent(self.sub_component1)
+        self.parent_component.add_subcomponent(self.sub_component2)
+        self.parent_component.add_subcomponent(self.sub_component3)
+
+    def test_mass_prop_buildup(self):
+        """Test providing mass properties during initialization."""
+
+        # Create mass properties for the parent component
+        self.parent_component.quantities.mass_properties.mass = Q_(0, 'kg')
+        self.parent_component.quantities.mass_properties.cg_vector = Vector(Q_([1, 0, 0], 'm'), self.axis)
+
+        # Create mass properties for the first subcomponent
+        self.sub_component1.quantities.mass_properties.mass = Q_(15, 'kg')
+        self.sub_component1.quantities.mass_properties.cg_vector = Vector(Q_([0, 1, 0], 'm'), self.axis)
+
+        # Create mass properties for the second subcomponent
+        self.sub_component2.quantities.mass_properties.mass = Q_(5, 'kg')
+        self.sub_component2.quantities.mass_properties.cg_vector = Vector(Q_([0, 0, 1], 'm'), self.axis)
+
+        # Create mass properties for the third subcomponent
+        self.sub_component3.quantities.mass_properties.mass = Q_(20, 'kg')
+        self.sub_component3.quantities.mass_properties.cg_vector = Vector(Q_([0, 1, 0], 'm'), self.axis)
+
+        # Recursively compute mass properties from the parent component
+        total_mp = self.parent_component.compute_mass_properties()
+
+
+        # Compute the total mass (as a Q_ quantity)
+        total_mass = (self.parent_component.quantities.mass_properties.mass +
+                    self.sub_component1.quantities.mass_properties.mass +
+                    self.sub_component2.quantities.mass_properties.mass +
+                    self.sub_component3.quantities.mass_properties.mass)
+
+        # Compute the weighted sum of the cg vectors. We assume that each cg_vector has an attribute
+        expected_cg = (
+            self.parent_component.quantities.mass_properties.mass.magnitude * np.array(self.parent_component.quantities.mass_properties.cg_vector.vector.value) +
+            self.sub_component1.quantities.mass_properties.mass.magnitude * np.array(self.sub_component1.quantities.mass_properties.cg_vector.vector.value) +
+            self.sub_component2.quantities.mass_properties.mass.magnitude * np.array(self.sub_component2.quantities.mass_properties.cg_vector.vector.value) +
+            self.sub_component3.quantities.mass_properties.mass.magnitude * np.array(self.sub_component3.quantities.mass_properties.cg_vector.vector.value)
+        ) / total_mass.magnitude
+
+
+        # Total mass should be the sum of the parent's and the subcomponents' masses
+        np.testing.assert_almost_equal(total_mp.mass.value, 40)
+        # Total CG should be the weighted average of the CGs of the parent and subcomponents
+        np.testing.assert_almost_equal(total_mp.cg_vector.vector.value, expected_cg)
+        print("Total CG:", total_mp.cg_vector.vector.value)
