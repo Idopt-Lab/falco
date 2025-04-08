81e272b7de641c5e422aa16887652ae17c149f71
Nate Shune <106626400+nshune@users.noreply.github.com>
2025-04-08 10:47:19 -0400

    Lateral Directional Stability

--- flight_simulator/core/dynamics/trim_stability.py
+++ flight_simulator/core/dynamics/trim_stability.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 
 
 @dataclass
-class TrimStabilityMetrics:
-    A_mat_long : csdl.Variable
+class LinearStabilityMetrics:
+    A_mat_longitudinal : csdl.Variable
     real_eig_short_period : csdl.Variable
     imag_eig_short_period : csdl.Variable
     nat_freq_short_period : csdl.Variable
@@ -17,3 +17,18 @@ class TrimStabilityMetrics:
     nat_freq_phugoid : csdl.Variable
     damping_ratio_phugoid : csdl.Variable
     time_2_double_phugoid : csdl.Variable
+
+    A_mat_lateral_directional : csdl.Variable
+    real_eig_spiral : csdl.Variable
+    imag_eig_spiral : csdl.Variable
+    nat_freq_spiral : csdl.Variable
+    damping_ratio_spiral : csdl.Variable
+    time_2_double_spiral : csdl.Variable
+
+    real_eig_dutch_roll : csdl.Variable
+    imag_eig_dutch_roll : csdl.Variable
+    nat_freq_dutch_roll : csdl.Variable
+    damping_ratio_dutch_roll : csdl.Variable
+    time_2_double_dutch_roll : csdl.Variable
+
+    
--- flight_simulator/core/vehicle/conditions/aircraft_conditions.py
+++ flight_simulator/core/vehicle/conditions/aircraft_conditions.py
@@ -3,7 +3,7 @@ from flight_simulator.core.dynamics.aircraft_states import AircraftStates
 from flight_simulator.core.loads.forces_moments import Vector, ForcesMoments
 from flight_simulator.core.dynamics.axis import Axis, ValidOrigins
 from flight_simulator.core.dynamics.axis_lsdogeo import AxisLsdoGeo
-from flight_simulator.core.dynamics.trim_stability import TrimStabilityMetrics
+from flight_simulator.core.dynamics.trim_stability import LinearStabilityMetrics
 from flight_simulator.core.vehicle.models.equations_of_motion.eom_model import SixDoFModel
 from flight_simulator.utils.euler_rotations import build_rotation_matrix
 from typing import Union, List, Tuple
@@ -195,9 +195,17 @@ class AircraftCondition(Condition):
             print('-----------------------------------')
         return total_forces, total_moments
 
-    def perform_linear_stability_analysis(self, print_output: bool = False) -> TrimStabilityMetrics:
+    def perform_linear_stability_analysis(self, print_output: bool = False) -> LinearStabilityMetrics:
+        
         m = self.component.quantities.mass_properties.mass.value
-        iyy = self.component.quantities.mass_properties.inertia_tensor.inertia_tensor.value[1, 1]
+        inertia_tensor = self.component.quantities.mass_properties.inertia_tensor.inertia_tensor
+        ixx = inertia_tensor.value[0, 0]
+        iyy = inertia_tensor.value[1, 1]
+        izz = inertia_tensor.value[2, 2]
+        ixz = inertia_tensor.value[0, 2]
+        ixy = inertia_tensor.value[0, 1]
+        iyz = inertia_tensor.value[1, 2]
+        
         total_forces = self.component.quantities.total_forces
         total_moments = self.component.quantities.total_moments
         if len(total_forces.shape) == 1:
@@ -207,17 +215,26 @@ class AircraftCondition(Condition):
         g0 = 9.81
         ac_states = self.quantities.ac_states
         for i in range(num_nodes):
-            A_mat = csdl.Variable(shape=(4, 4), value=0.)
+            A_mat_L = csdl.Variable(shape=(4, 4), value=0.)
+
             u = ac_states.states.u
+            v = ac_states.states.v
             w = ac_states.states.w
+            p = ac_states.states.p
             q = ac_states.states.q
+            r = ac_states.states.r
             theta = ac_states.states.theta
+
+            # Aerodynamic Forces and Moments
             X = total_forces[i, 0]
             Y = total_forces[i, 1]
             Z = total_forces[i, 2]
             L = total_moments[i, 0]
             M = total_moments[i, 1]
             N = total_moments[i, 2]
+
+            # Longitudinal Stability Derivatives
+            
             X_u = csdl.derivative(ofs=X, wrts=u)
             X_w = csdl.derivative(ofs=X, wrts=w)
             X_q = csdl.derivative(ofs=X, wrts=q)
@@ -227,35 +244,84 @@ class AircraftCondition(Condition):
             M_u = csdl.derivative(ofs=M, wrts=u)
             M_w = csdl.derivative(ofs=M, wrts=w)
             M_q = csdl.derivative(ofs=M, wrts=q)
-            A_mat = A_mat.set(csdl.slice[0, 0], X_u / m)
-            A_mat = A_mat.set(csdl.slice[0, 1], X_w / m)
-            A_mat = A_mat.set(csdl.slice[0, 3], -g0 * csdl.cos(theta))
-            A_mat = A_mat.set(csdl.slice[1, 0], Z_u / m)
-            A_mat = A_mat.set(csdl.slice[1, 1], Z_w / m)
-            A_mat = A_mat.set(csdl.slice[1, 2], (Z_q + m * u) / m)
-            A_mat = A_mat.set(csdl.slice[1, 3], -g0 * csdl.sin(theta))
-            A_mat = A_mat.set(csdl.slice[2, 0], M_u / iyy)
-            A_mat = A_mat.set(csdl.slice[2, 1], M_w / iyy)
-            A_mat = A_mat.set(csdl.slice[2, 2], M_q / iyy)
-            A_mat = A_mat.set(csdl.slice[3, 2], 1.)
-        eig_val_operation = EigValOperation()
-        eig_real, eig_imag = eig_val_operation.evaluate(A_mat)
+
+            # Longitudinal A Matrix
+            A_mat_L = A_mat_L.set(csdl.slice[0, 0], X_u / m)
+            A_mat_L = A_mat_L.set(csdl.slice[0, 1], X_w / m)
+            A_mat_L = A_mat_L.set(csdl.slice[0, 3], -g0 * csdl.cos(theta))
+            A_mat_L = A_mat_L.set(csdl.slice[1, 0], Z_u / m)
+            A_mat_L = A_mat_L.set(csdl.slice[1, 1], Z_w / m)
+            A_mat_L = A_mat_L.set(csdl.slice[1, 2], (Z_q + m * u) / m)
+            A_mat_L = A_mat_L.set(csdl.slice[1, 3], -g0 * csdl.sin(theta))
+            A_mat_L = A_mat_L.set(csdl.slice[2, 0], M_u / iyy)
+            A_mat_L = A_mat_L.set(csdl.slice[2, 1], M_w / iyy)
+            A_mat_L = A_mat_L.set(csdl.slice[2, 2], M_q / iyy)
+            A_mat_L = A_mat_L.set(csdl.slice[3, 2], 1.)
+
+            # Lateral-Directional Stability Derivatives
+            Y_v = csdl.derivative(ofs=Y, wrts=v)
+            Y_p = csdl.derivative(ofs=Y, wrts=p)
+            Y_r = csdl.derivative(ofs=Y, wrts=r)
+            L_v = csdl.derivative(ofs=L, wrts=v)
+            L_p = csdl.derivative(ofs=L, wrts=p)
+            L_r = csdl.derivative(ofs=L, wrts=r)
+            N_v = csdl.derivative(ofs=N, wrts=v)
+            N_p = csdl.derivative(ofs=N, wrts=p)
+            N_r = csdl.derivative(ofs=N, wrts=r)
+
+            # Lateral-Directional A Matrix
+            xi = ixx*izz - ixz**2
+            A_mat_LD = csdl.Variable(shape=(4, 4), value=0.)
+            A_mat_LD = A_mat_LD.set(csdl.slice[0, 0], Y_v / m)
+            A_mat_LD = A_mat_LD.set(csdl.slice[0, 1], Y_p / m)
+            A_mat_LD = A_mat_LD.set(csdl.slice[0, 2], Y_r / m - u)
+            A_mat_LD = A_mat_LD.set(csdl.slice[0, 3], g0 * csdl.cos(theta))
+            A_mat_LD = A_mat_LD.set(csdl.slice[1, 0], 1/xi * (izz*L_v + ixz*N_v))
+            A_mat_LD = A_mat_LD.set(csdl.slice[1, 1], 1/xi * (izz*L_p + ixz*N_p))
+            A_mat_LD = A_mat_LD.set(csdl.slice[1, 2], 1/xi * (izz*L_r + ixz*N_r))
+            A_mat_LD = A_mat_LD.set(csdl.slice[2, 0], 1/xi * (ixz*L_v + ixx*N_v))
+            A_mat_LD = A_mat_LD.set(csdl.slice[2, 1], 1/xi * (ixz*L_p + ixx*N_p))
+            A_mat_LD = A_mat_LD.set(csdl.slice[2, 2], 1/xi * (ixz*L_r + ixx*N_r))
+            A_mat_LD = A_mat_LD.set(csdl.slice[3, 1], 1.)
+            A_mat_LD = A_mat_LD.set(csdl.slice[3, 2], csdl.tan(theta))
+
+                                
+        eig_val_long_operation = EigenValueOperationLongitudinal()
+        eig_real_long, eig_imag_long = eig_val_long_operation.evaluate(A_mat_L)
+        eig_val_lat_operation = EigenValueOperationLateralDirectional()
+        eig_real_lat, eig_imag_lat = eig_val_lat_operation.evaluate(A_mat_LD)
+
         
         # Short period
-        lambda_sp_real = eig_real[0]
-        lambda_sp_imag = eig_imag[0]
+        lambda_sp_real = eig_real_long[0]
+        lambda_sp_imag = eig_imag_long[0]
         sp_omega_n = ((lambda_sp_real ** 2 + lambda_sp_imag ** 2) + 1e-10) ** 0.5
         sp_damping_ratio = -lambda_sp_real / sp_omega_n
         sp_time_2_double = np.log(2) / ((lambda_sp_real ** 2 + 1e-10) ** 0.5)
 
         # Phugoid
-        lambda_phugoid_real = eig_real[1]
-        lambda_phugoid_imag = eig_imag[1]
+        lambda_phugoid_real = eig_real_long[1]
+        lambda_phugoid_imag = eig_imag_long[1]
         phugoid_omega_n = ((lambda_phugoid_real ** 2 + lambda_phugoid_imag ** 2) + 1e-10) ** 0.5
         phugoid_damping_ratio = -lambda_phugoid_real / phugoid_omega_n
         phugoid_time_2_double = np.log(2) / ((lambda_phugoid_real ** 2 + 1e-10) ** 0.5)
-        self.quantities.stability_analysis = TrimStabilityMetrics(
-            A_mat_long=A_mat,
+
+        # Spiral
+        lambda_spiral_real = eig_real_lat[0]
+        lambda_spiral_imag = eig_imag_lat[0]
+        spiral_omega_n = ((lambda_spiral_real ** 2 + lambda_spiral_imag ** 2) + 1e-10) ** 0.5
+        spiral_damping_ratio = -lambda_spiral_real / spiral_omega_n
+        spiral_time_2_double = np.log(2) / ((lambda_spiral_real ** 2 + 1e-10) ** 0.5)
+
+        # Dutch Roll
+        lambda_dutch_roll_real = eig_real_lat[1]
+        lambda_dutch_roll_imag = eig_imag_lat[1]
+        dutch_roll_omega_n = ((lambda_dutch_roll_real ** 2 + lambda_dutch_roll_imag ** 2) + 1e-10) ** 0.5
+        dutch_roll_damping_ratio = -lambda_dutch_roll_real / dutch_roll_omega_n
+        dutch_roll_time_2_double = np.log(2) / ((lambda_dutch_roll_real ** 2 + 1e-10) ** 0.5)
+
+        self.quantities.stability_analysis = LinearStabilityMetrics(
+            A_mat_longitudinal=A_mat_L,
             real_eig_short_period=lambda_sp_real,
             imag_eig_short_period=lambda_sp_imag,
             nat_freq_short_period=sp_omega_n,
@@ -265,12 +331,23 @@ class AircraftCondition(Condition):
             imag_eig_phugoid=lambda_phugoid_imag,
             nat_freq_phugoid=phugoid_omega_n,
             damping_ratio_phugoid=phugoid_damping_ratio,
-            time_2_double_phugoid=phugoid_time_2_double
+            time_2_double_phugoid=phugoid_time_2_double,
+            A_mat_lateral_directional=A_mat_LD,
+            real_eig_spiral=lambda_spiral_real,
+            imag_eig_spiral=lambda_spiral_imag,
+            nat_freq_spiral=spiral_omega_n,
+            damping_ratio_spiral=spiral_damping_ratio,
+            time_2_double_spiral=spiral_time_2_double,
+            real_eig_dutch_roll=lambda_dutch_roll_real,
+            imag_eig_dutch_roll=lambda_dutch_roll_imag,
+            nat_freq_dutch_roll=dutch_roll_omega_n,
+            damping_ratio_dutch_roll=dutch_roll_damping_ratio,
+            time_2_double_dutch_roll=dutch_roll_time_2_double
         )
         if print_output:
             print('-----------------------------------')
             print('Longitudinal Stability Metrics for:', self.__class__.__name__, 'in the Flight Dynamics Axis:')
-            print('A Matrix:', self.quantities.stability_analysis.A_mat_long.value)
+            print('A Matrix:', self.quantities.stability_analysis.A_mat_longitudinal.value)
             print('Short Period Real Eigenvalue:', self.quantities.stability_analysis.real_eig_short_period.value)
             print('Short Period Imaginary Eigenvalue:', self.quantities.stability_analysis.imag_eig_short_period.value)
             print('Short Period Natural Frequency:', self.quantities.stability_analysis.nat_freq_short_period.value)
@@ -285,26 +362,32 @@ class AircraftCondition(Condition):
         return self.quantities.stability_analysis
 
 
-class EigValOperation(csdl.CustomExplicitOperation):
+class EigenValueOperationLongitudinal(csdl.CustomExplicitOperation):
     def __init__(self):
         super().__init__()
 
     def evaluate(self, mat):
         shape = mat.shape
         size = shape[0]
+
         self.declare_input("mat", mat)
-        eig_real = self.create_output("eig_vals_real", shape=(size,))
-        eig_imag = self.create_output("eig_vals_imag", shape=(size,))
+        eig_real = self.create_output("eig_vals_real", shape=(size, ))
+        eig_imag = self.create_output("eig_vals_imag", shape=(size, ))
+
         self.declare_derivative_parameters("eig_vals_real", "mat")
         self.declare_derivative_parameters("eig_vals_imag", "mat")
+
         return eig_real, eig_imag
 
     def compute(self, inputs, outputs):
         mat = inputs["mat"]
+
         eig_vals, eig_vecs = np.linalg.eig(mat)
+    
         idx = np.abs(eig_vals).argsort()[::-1]
         eig_vals = eig_vals[idx]
         eig_vecs = eig_vecs[:, idx]
+
         outputs['eig_vals_real'] = np.real(eig_vals)
         outputs['eig_vals_imag'] = np.imag(eig_vals)
 
@@ -315,18 +398,84 @@ class EigValOperation(csdl.CustomExplicitOperation):
         idx = np.abs(eig_vals).argsort()[::-1]
         eig_vals = eig_vals[idx]
         eig_vecs = eig_vecs[:, idx]
+
+        # v inverse transpose
         v_inv_T = (np.linalg.inv(eig_vecs)).T
-        temp_r = np.zeros((size, size * size))
-        temp_i = np.zeros((size, size * size))
+
+        # preallocate Jacobian: n outputs, n^2 inputs
+        temp_r = np.zeros((size, size*size))
+        temp_i = np.zeros((size, size*size))
+
         for j in range(len(eig_vals)):
+            # dA/dw(j,:) = v(:,j)*(v^-T)(:j)
             partial = np.outer(eig_vecs[:, j], v_inv_T[:, j]).flatten(order='F')
+            # Note that the order of flattening matters, hence argument in flatten()
+
+            # Set jacobian rows
             temp_r[j, :] = np.real(partial)
             temp_i[j, :] = np.imag(partial)
+
+        # Set Jacobian
         derivatives['eig_vals_real', 'mat'] = temp_r
         derivatives['eig_vals_imag', 'mat'] = temp_i
 
 
+class EigenValueOperationLateralDirectional(csdl.CustomExplicitOperation):
+    def __init__(self):
+        super().__init__()
 
+    def evaluate(self, mat):
+        shape = mat.shape
+        size = shape[0]
+
+        self.declare_input("mat", mat)
+        eig_real = self.create_output("eig_vals_real", shape=(size, ))
+        eig_imag = self.create_output("eig_vals_imag", shape=(size, ))
+
+        self.declare_derivative_parameters("eig_vals_real", "mat")
+        self.declare_derivative_parameters("eig_vals_imag", "mat")
+
+        return eig_real, eig_imag
+
+    def compute(self, inputs, outputs):
+        mat = inputs["mat"]
+
+        eig_vals, eig_vecs = np.linalg.eig(mat)
+    
+        idx = np.abs(eig_vals).argsort()[::-1]
+        eig_vals = eig_vals[idx]
+        eig_vecs = eig_vecs[:, idx]
+
+        outputs['eig_vals_real'] = np.real(eig_vals)
+        outputs['eig_vals_imag'] = np.imag(eig_vals)
+
+    def compute_derivatives(self, inputs, outputs, derivatives):
+        mat = inputs["mat"]
+        size = mat.shape[0]
+        eig_vals, eig_vecs = np.linalg.eig(mat)
+        idx = np.abs(eig_vals).argsort()[::-1]
+        eig_vals = eig_vals[idx]
+        eig_vecs = eig_vecs[:, idx]
+
+        # v inverse transpose
+        v_inv_T = (np.linalg.inv(eig_vecs)).T
+
+        # preallocate Jacobian: n outputs, n^2 inputs
+        temp_r = np.zeros((size, size*size))
+        temp_i = np.zeros((size, size*size))
+
+        for j in range(len(eig_vals)):
+            # dA/dw(j,:) = v(:,j)*(v^-T)(:j)
+            partial = np.outer(eig_vecs[:, j], v_inv_T[:, j]).flatten(order='F')
+            # Note that the order of flattening matters, hence argument in flatten()
+
+            # Set jacobian rows
+            temp_r[j, :] = np.real(partial)
+            temp_i[j, :] = np.imag(partial)
+
+        # Set Jacobian
+        derivatives['eig_vals_real', 'mat'] = temp_r
+        derivatives['eig_vals_imag', 'mat'] = temp_i
 class CruiseCondition(AircraftCondition):
     """Cruise condition: intended for steady analyses.
 
