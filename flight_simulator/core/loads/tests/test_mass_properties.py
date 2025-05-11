473d093202993687aa01128e3ce4d95b56794d0c
Darshan Sarojini <85709297+darshansarojini@users.noreply.github.com>
2025-05-11 18:15:50 -0400

    Update test_mass_properties.py

--- flight_simulator/core/loads/tests/test_mass_properties.py
+++ flight_simulator/core/loads/tests/test_mass_properties.py
@@ -15,7 +15,9 @@ class TestMassProperties(TestCase):
 
         self.axis = Axis(
             name='Inertial Axis',
-            translation=np.array([0, 0, 0]) * ureg.meter,
+            x=np.array([0, ]) * ureg.meter,
+            y=np.array([0, ]) * ureg.meter,
+            z=np.array([0, ]) * ureg.meter,
             phi=np.array([0, ]) * ureg.degree,
             theta=np.array([0, ]) * ureg.degree,
             psi=np.array([0, ]) * ureg.degree,
