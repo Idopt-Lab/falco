2d3e759511c2759abb45199048bdcda5bef9fbb8
Darshan Sarojini <85709297+darshansarojini@users.noreply.github.com>
2025-05-11 19:15:04 -0400

    Added tests for derivative of rotation matrix wrt angles

--- flight_simulator/utils/tests/test_euler_rotations.py
+++ flight_simulator/utils/tests/test_euler_rotations.py
@@ -21,8 +21,7 @@ class TestEulerRotations(unittest.TestCase):
         np.testing.assert_array_almost_equal(R.value, expected_R)
     
     def test_build_rotation_matrix_2(self):
-        recorder = csdl.Recorder(inline=True)
-        recorder.start()
+
         angles = csdl.Variable(shape=(3,), value=np.array([0., 0., np.pi/2]))
         seq = np.array([3, 2, 1])
         R = build_rotation_matrix(angles, seq)
@@ -32,8 +31,7 @@ class TestEulerRotations(unittest.TestCase):
         np.testing.assert_array_almost_equal(R.value, expected_R)
 
     def test_build_rotation_matrix_3(self):
-        recorder = csdl.Recorder(inline=True)
-        recorder.start()
+
         angles = csdl.Variable(shape=(3,), value=np.array([np.pi/2, 0., 0.]))
         seq = np.array([3, 2, 1])
         R = build_rotation_matrix(angles, seq)
@@ -43,8 +41,7 @@ class TestEulerRotations(unittest.TestCase):
         np.testing.assert_array_almost_equal(R.value, expected_R)
 
     def test_build_rotation_matrix_4(self):
-        recorder = csdl.Recorder(inline=True)
-        recorder.start()
+
         angles = csdl.Variable(shape=(3,), value=np.array([np.pi/4, np.pi/2, np.pi/6]))
         seq = np.array([3, 2, 1])
         R = build_rotation_matrix(angles, seq)
@@ -53,7 +50,12 @@ class TestEulerRotations(unittest.TestCase):
                                [-1., 0., 0.]])
         np.testing.assert_array_almost_equal(R.value, expected_R)
 
+    def test_derivative_of_rotation_matrix(self):
+        angles = csdl.Variable(shape=(3,), value=np.array([np.pi / 4, np.pi / 3, np.pi / 6]))
+        seq = np.array([3, 2, 1])
+        R = build_rotation_matrix(angles, seq)
 
+        dRda = csdl.derivative(R, angles)
 
-
-
+        np.testing.assert_array_almost_equal(dRda[csdl.slice[6, 1]].value, -np.cos(np.pi / 3))
+        np.testing.assert_array_almost_equal(dRda[csdl.slice[0, 0]].value, -np.sin(np.pi/4)*np.cos(np.pi/3))
