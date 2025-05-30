f96a641aad463f97764659e526b3350fb0f5aa23
Darshan Sarojini <85709297+darshansarojini@users.noreply.github.com>
2025-05-30 17:58:23 -0400

    Update tests_x57.py

---
+++ examples/advanced_examples/x57/tests_x57.py
@@ -0,0 +1,26 @@
+
+
+
+
+# Mass properties alone test
+# - Loads due to mass properties at 0 pitch angle
+# - Loads due to mass properties at non-0 pitch angle
+
+# Aero alone test
+# - Aero produces loads at wind axis -> wing axis -> FD body-fixed axis
+# - L/D when cruise motors are off < L/D when cruise motors are on
+# - CL when HLP are off << CL when HLP are on
+
+# Prop alone test
+# - If CM axis location is offset from FD axis, there is a pitch moment
+# - CM left is on; CM right is off. There is a yaw moment
+# - When both CM are ok; there is no yaw moment
+
+
+
+
+
+
+
+# todo: after Aviation paper
+# - Test parameterization solver when aircraft components are defined in a heirarchy
